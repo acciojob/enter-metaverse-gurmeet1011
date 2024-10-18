d54f25b1627ef5eacbf49195c98f0d99cbaec050
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-18 18:51:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-let text=document.getElementById("para")
+let text=document.getElementById("status")
 function change(s){
 	text.innerHTML="<h1>Entered Metaverse</h1>";
 }
